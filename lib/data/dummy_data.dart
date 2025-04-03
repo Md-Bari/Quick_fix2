f44cf973a3ce04df4d4ea3527ddeeb67a9aeb9cd
jannatul373 <ferdaus15-4982@diu.edu.bd>
2025-04-03 21:46:19 +0600

    d5

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -2,50 +2,50 @@ import 'package:flutter/material.dart';
 
 import '../models/models.dart';
 
-final List<ShoeModel> availableShoes = [
-  ShoeModel(
+final List<Category> availableShoes = [
+  Category(
     name: "Maid",
     model: "Mr cleaner",
-    price: 130.00,
+    price: 1500,
     imgAddress: "assets/images/maid.png",
     modelColor: const Color(0xffDE0106),
   ),
-  ShoeModel(
+  Category(
     name: "Plumber",
     model: "Mr Plumber Organization",
-    price: 500.00,
+    price: 500,
     imgAddress: "assets/images/plumber.png",
     modelColor: const Color(0xff3F7943),
   ),
-  ShoeModel(
+  Category(
     name: "Electrician",
     model: "JK Electronics",
-    price: 160.00,
+    price: 250,
     imgAddress: "assets/images/electrician.png",
     modelColor: const Color(0xffE66863),
   ),
-  ShoeModel(
+  Category(
     name: "AC Servicing",
     model: "Gree AC service",
-    price: 110.00,
+    price: 1000,
     imgAddress: "assets/images/ac_repair.png",
     modelColor: const Color(0xffD7D8DC),
   ),
-  ShoeModel(
+  Category(
     name: "Security Service",
     model: "DIU security Company",
     price: 150.00,
     imgAddress: "assets/images/security_service.png",
     modelColor: const Color(0xff37376B),
   ),
-  ShoeModel(
+  Category(
     name: "Beautician",
     model: "Persona body and hair salon",
     price: 115.00,
     imgAddress: "assets/images/beautician.png",
     modelColor: const Color(0xffE4E3E8),
   ),
-  ShoeModel(
+  Category(
     name: "Tv Service",
     model: "MK tv service centre",
     price: 150.00,
@@ -54,7 +54,7 @@ final List<ShoeModel> availableShoes = [
   ),
 ];
 
-List<ShoeModel> itemsOnBag = [];
+List<Category> itemsOnBag = [];
 
 final List<UserStatus> userStatus = [
   UserStatus(
