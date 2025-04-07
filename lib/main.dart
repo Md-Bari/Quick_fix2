bf8f96d840d648fd3ca49aacf9edf756b2bc4972
jannatul373 <ferdaus15-4982@diu.edu.bd>
2025-04-07 19:46:16 +0600

    fire

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:sneakers_app/view/navigator.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyBlw4K_YbsWBeJLHNIQ1WBoQYPYMmCPdPA",
+            authDomain: "fire-setup-932f2.firebaseapp.com",
+            projectId: "fire-setup-932f2",
+            storageBucket: "fire-setup-932f2.firebasestorage.app",
+            messagingSenderId: "73098558108",
+            appId: "1:73098558108:web:47defa528adb795b0d0b59"));
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
